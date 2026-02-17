8c233c36c668089f726064f653c9f5d0637634d3
jayeshmour44 <jayeshagarwa@gmail.com>
2026-02-17 16:49:08 +0530

    add modal container in project section

--- src/Components/Work/Work.jsx
+++ src/Components/Work/Work.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { projects } from '../../constant'
 
 export default function Work() {
+  
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const handleOpenModal = (project) => {
+    setSelectedProject(project);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedProject(null);
+  };
+
+  useEffect(() => {
+  if (selectedProject) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
+  }
+}, [selectedProject]);
+
   return (
     <section
     id='work'
@@ -21,6 +40,7 @@ export default function Work() {
         {projects.map((project) => (
           <div
           key={project.id}
+          onClick={() => handleOpenModal(project)}
           className='border border-white bg-gray-900 backdrop-blur-md rounded-2xl shadow-2xl overflow-hidden cursor-pointer hover:shadow-purple-500/50 hover:-translate-y-2 transition-transform duration-300'
           >
             <div className='p-4'>
@@ -49,6 +69,67 @@ export default function Work() {
           </div>
         ))}
       </div>
+      {/* Modal Container */}
+      {selectedProject && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/90 p-4">
+          <div className="bg-gray-900 rounded-xl shadow-2xl lg:w-full w-[90%] max-w-3xl max-h-[90vh] overflow-y-auto relative">
+            <div className="flex justify-end p-4">
+              <button
+                onClick={handleCloseModal}
+                className="text-white text-3xl font-bold hover:text-purple-500"
+              >
+                &times;
+              </button>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="w-full flex justify-center bg-gray-900 px-4">
+                <img
+                  src={selectedProject.image}
+                  alt={selectedProject.title}
+                  className="lg:w-full w-[95%] max-h-[400px] object-contain rounded-xl shadow-2xl"
+                />
+              </div>
+              <div className="lg:p-8 p-6">
+                <h3 className="lg:text-3xl font-bold text-white mb-4 text-md">
+                  {selectedProject.title}
+                </h3>
+                <p className="text-gray-400 mb-6 lg:text-base text-xs">
+                  {selectedProject.description}
+                </p>
+                <div className="flex flex-wrap gap-2 mb-6">
+                  {selectedProject.tags.map((tag, index) => (
+                    <span
+                      key={index}
+                      className="bg-[#251f38] text-xs font-semibold text-purple-500 rounded-full px-2 py-1"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+                <div className="flex gap-4">
+                  <a
+                    href={selectedProject.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-1/2 bg-gray-800 hover:bg-purple-800 text-gray-400 lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
+                  >
+                    View Code
+                  </a>
+                  <a
+                    href={selectedProject.webapp}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-1/2 bg-purple-600 hover:bg-purple-800 text-white lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
+                  >
+                    View Live
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   )
 }
